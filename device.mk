bd1b34459934a4a11c181057d1ff676b5a95d6ac
bengris32 <bengris32@protonmail.ch>
2024-08-31 15:07:58 +0100

    selene: Build 64-bit variant of libmemunreachable.vendor
    * This WAS previously being built by default, until we stopped
      building 64-bit audio libraries.
    
    Change-Id: Ife41331a0ffffb9b80da64b9f3f3413bb045b0f9
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -99,6 +99,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.6.vendor \
     android.hardware.camera.provider@2.6.vendor \
     libcamera_metadata.vendor \
+    libmemunreachable.vendor \
     libexif.vendor
 
 # ConsumerIr
