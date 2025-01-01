5065c4cae2d1a92c8bc87d4bd4e410ee5b9c9a42
atharvnegi <atharvnegi2951@gmail.com>
2025-01-01 16:47:08 +0530

    selene: Initial Skeleton Trees
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Android device tree for Redmi 10/10 Prime (selene)
+
+```
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ lineage_selene.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from selene device
+$(call inherit-product, device/xiaomi/selene/device.mk)
+
+PRODUCT_DEVICE := selene
+PRODUCT_NAME := lineage_selene
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 10
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="selene-user 13 TP1A.220624.014 V14.0.8.0.TKUINXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/selene/selene:12/TP1A.220624.014/V14.0.8.0.TKUINXM:user/release-keys
