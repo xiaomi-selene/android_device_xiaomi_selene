8ebdff7a081ddd2b50f5aef86a3ad9ef67e32e80
atharvnegi <atharvnegi2951@gmail.com>
2025-01-07 13:13:23 +0530

    selene: Configure Gatekeeper stack
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -96,6 +96,11 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder.vendor
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
