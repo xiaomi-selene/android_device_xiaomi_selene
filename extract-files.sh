ea511370a84138c80909e1ce7b44d770c5886260
atharvnegi <atharvnegi2951@gmail.com>
2025-06-04 06:18:58 +0530

    selene: Update IMS stack from fire `V816.0.5.0.UMXINXM`
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -59,9 +59,9 @@ fi
 
 function blob_fixup {
     case "$1" in
-        system/lib64/libsink.so)
+	system_ext/lib64/libsink.so)
             "${PATCHELF}" --add-needed "libshim_sink.so" "$2"
-	    ;;
+            ;;
         vendor/lib*/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
