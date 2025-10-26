205019db41e078ff367879cebcbe6d196f2cec90
atharvnegi <atharvnegi2951@gmail.com>
2025-10-26 15:37:56 +0530

    selene: Move to Xiaomi Fingerprint AIDL HAL
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -141,7 +141,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     com.fingerprints.extension@1.0.vendor \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    android.hardware.biometrics.fingerprint-service.xiaomi \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 # FM
