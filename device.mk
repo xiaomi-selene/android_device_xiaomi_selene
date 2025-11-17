d10579ce67995ed916cd7f4cb47b23c54d86ec82
claxten10 <claxten10@gmail.com>
2025-11-17 22:52:30 +0000

    selene: Import zumapro usb init.rc
    Change-Id: Iba0e19ef647bb4a5a0bc4566741a982fd9357f52
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -173,6 +173,7 @@ PRODUCT_PACKAGES += \
     init.modem.rc \
     init.mt6768.rc \
     init.mt6768.power.rc \
+    init.mt6768.usb.rc \
     init.project.rc \
     init.sensor_1_0.rc \
     init.target.rc \
